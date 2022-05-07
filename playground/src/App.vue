b61a52814a3f61d59eb7f16de4d18560c304d352
alexzhang <1642114555@qq.com>
2022-05-07 13:16:51 +0800

    feat: add monorepo

---
+++ playground/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>USD-UI Playground</h1>
+  <h3>Here is the component ⬇️</h3>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
